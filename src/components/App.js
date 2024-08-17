c107a6f48e2fe174e088cfd5cee99ad08897159e
norah seini kinyamasyo <norahkinyamasyo@moringaschool.com>
2024-08-17 22:41:34 +0300

    labs complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import blogData from "../data/blog";
-
-console.log(blogData);
+import About from "./About";
+import Article from "./Article";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
+      <About />
+      <Article />
     </div>
   );
 }
